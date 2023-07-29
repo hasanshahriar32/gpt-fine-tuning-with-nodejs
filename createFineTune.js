414c6a587c9db24fc057f42ff3b3f29db3d7e0c4
Mr. Hasan <hasanshahriar32@gmail.com>
2023-07-29 10:15:58 +0600

    updated

--- createFineTune.js
+++ createFineTune.js
@@ -5,11 +5,11 @@ async function createFineTune() {
   try {
     const response = await openai.createFineTune({
       training_file: fileId,
-      model: "davinci:ft-sj-innovation-2023-07-22-00-14-19",
+      model: "davinci",
       batch_size: 1, // Set the desired batch size
       prompt_loss_weight: 0.01,
       learning_rate_multiplier: 0.1, // Set the desired learning rate
-      n_epochs: 1, // Set the desired number of epochs
+      n_epochs: 3, // Set the desired number of epochs
     });
     console.log("response: ", response);
   } catch (err) {
